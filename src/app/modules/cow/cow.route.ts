8b79d4e96bfd380c8343a1dbfee82fe91eb30e19
JfJubaer <skk5727@gmail.com>
2024-04-19 14:20:56 +0600

    done

--- src/app/modules/cow/cow.route.ts
+++ src/app/modules/cow/cow.route.ts
@@ -2,21 +2,37 @@ import express from 'express';
 import { cowValidation } from './cow.validation';
 import validateRequest from '../../middlewares/validateRequest';
 import { CowController } from './cow.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/enum';
 
 const router = express.Router();
 
 router.post(
   '/',
   validateRequest(cowValidation.cowValidationSchema),
+  auth(ENUM_USER_ROLE.SELLER),
   CowController.createCow,
 );
-router.get('/:id', CowController.getOneCow);
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.SELLER),
+  CowController.getOneCow,
+);
 router.patch(
   '/:id',
   validateRequest(cowValidation.cowUpdateValidationSchema),
+  auth(ENUM_USER_ROLE.SELLER),
   CowController.updateOneCow,
 ),
-  router.delete('/:id', CowController.deleteOneCow),
-  router.get('/', CowController.getAllcows);
+  router.delete(
+    '/:id',
+    auth(ENUM_USER_ROLE.SELLER),
+    CowController.deleteOneCow,
+  ),
+  router.get(
+    '/',
+    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.SELLER),
+    CowController.getAllcows,
+  );
 
 export const CowRoutes = router;
--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -2,22 +2,26 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserValidation } from './user.validation';
 import { UserController } from './user.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/enum';
 const router = express.Router();
 
 router.post(
   '/auth/signup',
   validateRequest(UserValidation.userValidationSchema),
+  auth(ENUM_USER_ROLE.ADMIN),
   UserController.createUser,
 );
-router.get('/:id', UserController.getOneUser);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getOneUser);
 
-router.delete('/:id', UserController.deleteOneUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteOneUser);
 
 router.patch(
   '/:id',
   validateRequest(UserValidation.userUpdateValidationSchema),
+  auth(ENUM_USER_ROLE.ADMIN),
   UserController.updateOneUser,
 );
-router.get('/', UserController.getAllusers);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllusers);
 
 export const UserRoutes = router;
